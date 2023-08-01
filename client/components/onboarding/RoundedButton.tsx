bb09afce28c080c23a1b342befc3785014c84d2d
Pete <peteburquette@gmail.com>
2023-08-01 22:02:09 +0100

    splash work

--- client/components/onboarding/RoundedButton.tsx
+++ client/components/onboarding/RoundedButton.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import React from 'react'
+import { Text, TouchableOpacity } from 'react-native'
+import { RFPercentage } from 'react-native-responsive-fontsize'
 
 interface PageProps {
-  label: string;
-  onPress: any;
+  label: string
+  onPress: any
 }
-
-const RoundedButton = ({label, onPress}: PageProps) => {
+const RoundedButton = ({ label, onPress }: PageProps) => {
   return (
     <TouchableOpacity
-      style={{alignItems: 'center', justifyContent: 'center'}}
-      onPress={onPress}>
-      <Text style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}>
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 58,
+        minWidth: 58,
+        padding: 10,
+      }}
+      onPress={onPress}
+    >
+      <Text
+        style={{
+          fontSize: RFPercentage(3.4),
+          color: 'white',
+          fontWeight: 'bold',
+        }}
+      >
         {label}
       </Text>
     </TouchableOpacity>
-  );
-};
+  )
+}
 
-export default RoundedButton;
+export default RoundedButton
